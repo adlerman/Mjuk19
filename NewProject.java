b3440db288eefcc1894f49bcdc994adafe6b2219
Vincent Adler <adler.adler92@gmail.com>
2019-09-25 14:12:28 +0200

    Added all my initial files for my java project.

---
+++ NewProject.java
@@ -0,0 +1,7 @@
+public class NewProject {
+  public static void main(String[]args) {
+    Player p1 = new Player("Göran");
+    Player p2 = new Player("Alma");
+
+  }
+}
---
+++ Player.java
@@ -0,0 +1,8 @@
+public class Player {
+  String name;
+  int playerNumber;
+  int score;
+public Player(String nameIn) {
+  name = nameIn;
+}
+}
